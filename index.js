15a56262a693ef68a75fbc9eff744ae4522c128b
Maciej Marosz <memgos@gmail.com>
2021-02-13 17:55:43 +0100

    enviroment variable

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express  = require('express');
-const port = proces.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 app.get('/', (req, res) => {
     res.send('Hello fallks! It is my app on vps! Good luck!');
