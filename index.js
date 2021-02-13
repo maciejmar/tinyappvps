3fed611813f6adf436071b3764af83c593af8391
Maciej Marosz <memgos@gmail.com>
2021-02-13 15:54:39 +0100

    first commit on app for vps

---
+++ index.js
@@ -0,0 +1,11 @@
+const express  = require('express');
+const port = 3000;
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello fallks! It is my app on vps! Good luck!');
+});
+app.get('/about', (req,res) => {
+    res.send('This is about of tinyappvps')
+});
+app.listen(port);
+console.log(`My app is on port ${ port} and is listening`);
